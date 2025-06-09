cbf8c49a72eac369a216cc6382f1a62550de11e6
disris <baironvillarberrios@gmail.com>
2025-06-09 01:36:01 -0400

    allow true

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -148,7 +148,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Configuración de CORS
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True  # Solo para desarrollo
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 CORS_ALLOWED_METHODS = [
